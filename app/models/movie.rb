462e4af7b36dfcd1d694201e3dee97f3eec48bfa
ana rebelo <anarebelosoares@gmail.com>
2022-02-17 16:02:42 +0100

    backend working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,4 @@ class Movie < ApplicationRecord
   has_many :bookmarks
   validates :title, uniqueness: true, presence: true
   validates :overview, uniqueness: true, presence: true
-  # You can’t delete a movie if it is referenced in at least one bookmark.
-
 end
